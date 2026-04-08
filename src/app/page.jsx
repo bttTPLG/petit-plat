f2b6db4bb18ba654c58cbda12d274889cad4ec8c
Baptiste COURTADON <b.courtadon@TPLG.vbx>
2026-04-08 14:29:07 +0200

    First commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h2>COUCOU LOULOU</h2>
+      </main>
+    </div>
+  );
+}
