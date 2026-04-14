f2f5974f3d15c88b2dfcf9c6026cc74731d49b50
Baptiste COURTADON <b.courtadon@TPLG.vbx>
2026-04-14 13:42:25 +0200

    Page recette

--- src/app/recipe/[id]/page.jsx
+++ src/app/recipe/[id]/page.jsx
@@ -1,11 +1,86 @@
 import styles from "@/styles/page.module.css";
+import "@/styles/recipe.css";
+import Image from "next/image";
+import recipes from "../../../../data/recipes.json";
 
-export default function Home() {
+export default async function RecipePage({ params }) {
+  const { id } = await params;
+
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) {
+    return <p>Recette introuvable</p>;
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles.filters_container}>
-          <h1>COUCOU</h1>
+        <div className={styles.grid_container}>
+          <Image
+            src={`/images/${recipe.image}`}
+            alt={`Photo de la recette ${recipe.name}`}
+            width={606}
+            height={738}
+            className="recipe-picture"
+          />
+          <div className="recipe-explain">
+            <h1>{recipe.name}</h1>
+            <div className="info">
+              <h2>Temps de préparation</h2>
+              <p className="time">{recipe.time}min</p>
+            </div>
+            <div className="info">
+              <h2>Ingrédients</h2>
+              <div className="list-container">
+                <ul>
+                  {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>
+                      <div className="element">
+                        <span>{ingredient.ingredient}</span>
+                        <p>
+                          {ingredient.quantity ? ingredient.quantity : "-"}
+                          {ingredient.unit ? ` ${ingredient.unit}` : ""}
+                        </p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="info">
+              <h2>Ustensiles nécessaires</h2>
+              <div className="list-container">
+                <ul>
+                  {recipe.ustensils.map((ustensil, index) => (
+                    <li key={index}>
+                      <div className="element">
+                        <span>{ustensil}</span>
+                        <p>1</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="info">
+              <h2>Appareils nécessaires</h2>
+              <div className="list-container">
+                <ul>
+                  <li>
+                    <div className="element">
+                      <span>{recipe.appliance}</span>
+                      <p>1</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="info">
+              <h2>Recette</h2>
+              <div className="description">
+                <p>{recipe.description}</p>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
